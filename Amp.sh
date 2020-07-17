52f3d2b37f21415a591180e34b93aebd4d0c10b3
yjiakang <49177016+yjiakang@users.noreply.github.com>
2020-07-17 17:32:17 +0800

    Update Amp.sh

--- Amp.sh
+++ Amp.sh
@@ -5,7 +5,7 @@ usage(){
 		-o [abs_output_dir | required]
 		-m [abs_sample_metadata.tsv | required]
 		-n [threads | defalut: 4]
-		-d [dada2/deblur | chooose one from above, default: dada2]
+		-d [dada2/deblur | choose one from above, default: dada2]
 		-a [silva_fl/silva_v4/gg_fl/gg_v4 | choose one from above, default: gg_v4]
 		-h [print this help info]"
 	exit -1
